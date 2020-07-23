7afcd90b4a2ebd20049e5648bda86061d6198270
Lix311 <alex.sterrantino@gmail.com>
2020-07-23 00:57:14 -0400

    auth done

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -1,3 +1,11 @@
 class Api::V1::AuthController < ApplicationController
-    
+    def login
+       user = User.find_by(username: params[:user][:username])
+
+       if user && user.authenticate(params[:user][:password])
+            render json: user
+       else 
+        render json: {errors: "You dun goofed"}
+        end
+    end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       
       post '/signup', to: 'users#create'
+      post '/login', to: 'auth#login'
      
     end
   end
