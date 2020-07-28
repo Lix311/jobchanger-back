b60428e205285db8a770f87cceab2f8461005fbd
Lix311 <alex.sterrantino@gmail.com>
2020-07-28 15:55:55 -0400

    fetch working

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,50 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'nokogiri'
+require 'net/http'
+
+def seed_positions(data)
+    data.each do |job|
+        Job.create(
+            company: job['company'],
+            location: job['location'],
+            title: job['title'],
+            logo: job['logo'],
+            description: job['description'],
+            date: job['date']
+        )
+    end 
+end 
+
+
+def api_call(page)
+  first_fifty = "https://jobs.github.com/positions.json"
+  next_page = first_fifty + "?page=" + page.to_s
+  if page == 0
+    uri = URI.parse(URI.escape(first_fifty))
+  else
+    uri = URI.parse(URI.escape(next_page))
+  end
+  response = Net::HTTP.get_response(uri)
+  data = JSON.parse(response.body)
+  seed_positions(data) # data is the json, you will probably want to write your own function here
+  return data
+end
+
+page = 1
+loop do
+  data = api_call(page)
+  page += 1
+  if data.empty? 
+    break
+  end
+end
+
+# require 'rest-client'
+
+# rm = RestClient.get `https://jobs.github.com/positions.json?description=ruby&page=1`
+
+
 
 
